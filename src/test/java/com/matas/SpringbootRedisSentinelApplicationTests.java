16680dc99d642aaa07162abc70ef780de99bd6e8
matas <mataszhang@163.com>
2018-10-17 17:46:42 +0800

    just failover

--- src/test/java/com/matas/SpringbootRedisSentinelApplicationTests.java
+++ src/test/java/com/matas/SpringbootRedisSentinelApplicationTests.java
@@ -2,15 +2,18 @@ package com.matas;
 
 import com.matas.redis.RedisService;
 import com.matas.redis.Student;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class SpringbootRedisSentinelApplicationTests {
 
     @Resource
@@ -28,10 +31,22 @@ public class SpringbootRedisSentinelApplicationTests {
         service.set(s);
 
         String name = service.get("myname");
-        System.out.println("name:"+name);
+        System.out.println("name:" + name);
 
         Student stu = service.getStudent("001");
         System.out.println(stu);
+    }
+
+    @Test
+    public void testFailover() {
+        while (true) {
+            try {
+                System.out.println(service.get("myname"));
+                TimeUnit.SECONDS.sleep(1);
+            } catch (Exception e) {
+                System.err.println("error=>" + e.getMessage());
+            }
+        }
     }
 
 }
